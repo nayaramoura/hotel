49003e3d4bac64b92590ba16d2c05361a3704b84
Nayara Moura <nayara.sannie@gmail.com>
2016-09-09 01:46:18 -0300

    Validate weekdays vs weekend

--- hotel.rb
+++ hotel.rb
@@ -1,5 +1,20 @@
+require 'Time'
+require 'pry'
+
 class Hotel
-  def search_cheapest (customer_type, date)
-    'Lakewood'
+
+  def search_cheapest (customer_type, string_date)
+    date = Time.parse(string_date)
+
+    if is_weekday(date) then
+      'Lakewood'
+    else
+      'Bridgewood'
+    end
   end
+
+  def is_weekday(date)
+    (1..5).include?(date.wday)
+  end
+
 end
--- hotel_test.rb
+++ hotel_test.rb
@@ -1,13 +1,27 @@
 require 'Rspec'
 require_relative 'hotel'
 
-describe 'Hotel' do
+describe '#hotel' do
 
-  context 'Given a correct input' do
+  context 'Regular customer' do
     let(:hotel) { Hotel.new }
 
-    it 'Returns the cheapest hotel when one date is informed' do
+    it 'Returns the cheapest hotel for one date on weekday' do
       hotel_name = hotel.search_cheapest('regular', '16-Mar-2017')
+      expect(hotel_name).to eq('Lakewood')
+    end
+
+    it 'Returns the cheapest hotel for one date on weekend' do
+      hotel_name = hotel.search_cheapest('regular', '18-Mar-2017')
+      expect(hotel_name).to eq('Bridgewood')
+    end
+  end
+
+  context 'Reward customer' do
+    let(:hotel) { Hotel.new }
+
+    it 'Returns the cheapest hotel for one date on weekday' do
+      hotel_name = hotel.search_cheapest('reward', '16-Mar-2017')
       expect(hotel_name).to eq('Lakewood')
     end
 
